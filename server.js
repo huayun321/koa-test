ce48e859d54b25ff650f2d62f70297eee68f47bf
huayun321 <huayun321@gmail.com>
2015-09-24 02:59:45 +0800

    chmod +x deploy, and fix the rsa what what problem

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.use(function *(){
     this.body = 'This koa2, hello world! And again! And again!!! And again!!!!\n';
     this.body += 'pm2 auto pull! pull again!!!\n';
     this.body += 'chmod +x deploy, and fix the rsa what what problem\n';
+    this.body += 'chmod +x deploy, and fix the rsa what what problem\n';
+
 });
 
 app.listen(3300);
