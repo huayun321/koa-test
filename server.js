9997cbaf80edce4a87daadeada08de0b9db75a38
huayun321 <huayun321@gmail.com>
2015-09-23 19:13:12 +0800

    3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = koa();
 
 app.use(function *(){
-    this.body = 'This koa2, hello world! And again! And again!!!';
+    this.body = 'This koa2, hello world! And again! And again!!! And again!!!!';
 
 });
 
