fb35851be7b4bea13d792dac6ad232d15e66c57f
huayun321 <huayun321@gmail.com>
2015-09-24 02:47:04 +0800

    chmod +x deploy, and fix the rsa what what problem

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var koa = require('koa');
 var app = koa();
 
 app.use(function *(){
-    this.body = 'This koa2, hello world! And again! And again!!! And again!!!!';
-    this.body += 'pm2 auto pull! pull again!!!'
-
+    this.body = 'This koa2, hello world! And again! And again!!! And again!!!!\n';
+    this.body += 'pm2 auto pull! pull again!!!\n';
+    this.body += 'chmod +x deploy, and fix the rsa what what problem\n';
 });
 
 app.listen(3300);
