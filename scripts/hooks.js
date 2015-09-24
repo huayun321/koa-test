54b063529b8d8357b184b86b6bf2b9635e15351c
huayun321 <huayun321@gmail.com>
2015-09-24 12:36:05 +0800

    change hooks secret

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var exec = require('child_process').exec;
 var createHandler = require('github-webhook-handler');
-var handler = createHandler({ path: '/', secret: 'baishan' });
+var handler = createHandler({ path: '/', secret: '∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗' });
 
 http.createServer(function(req, res) {
     handler(req, res, function (err) {
