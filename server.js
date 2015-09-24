d0d0dba5cc7e3405b5f1d103854cffe556cf27e7
huayun321 <huayun321@gmail.com>
2015-09-24 12:42:48 +0800

    change hooks secret

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ var app = koa();
 app.use(function *(){
     this.body = 'This koa2, hello world! \n';
     this.body += 'gen ssh key , then set to github, then make a webhooks, \n';
-    this.body += 'chmod +x deploy, and fix the rsa what what problem\n';
-    this.body += 'chmod +x deploy, and fix the rsa what what problem\n';
-    this.body += 'chmod +x deploy, and fix the rsa what what problem\n';
-    this.body += 'chmod +x deploy, and fix the rsa what what problem\n';
+
 
 
 
